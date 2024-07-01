dc65e44aec4361c612f62bf4f308a4af9cd13714
Zafran <mzafransultani56@gmail.com>
2024-07-01 10:42:21 +0500

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Zafran on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
